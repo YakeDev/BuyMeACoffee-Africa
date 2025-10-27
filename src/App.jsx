10c013819ed2646b8c9ad118a86bc6b2bfa1127c
Jules MUKADI <146131402+2MJ-DEV@users.noreply.github.com>
2025-10-27 18:26:39 +0200

    Fix: backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { supabase } from "../server/src/lib/supabaseClient";
 import AppRouter from "./routes/AppRouter";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data, error } = await supabase.from("users").select("*");
+      if (error) console.error("Erreur Supabase :", error);
+      else console.log("Users :", data);
+    };
+    fetchUsers();
+  }, []);
+
   return <AppRouter />;
 };
 
